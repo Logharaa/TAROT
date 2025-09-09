15574d0ef3d44155c490baf98690666cdb31a08c
Gaël Meheust <gaelmeheust78@gmail.com>
2025-09-09 16:29:03 +0200

    Implement pagination

--- overview/views.py
+++ overview/views.py
@@ -2,4 +2,15 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, "overview/index.html", {})
+    # Get the telescope ID from the query string
+    telescope_id = int(request.GET.get("telescope_id", 1))
+
+    print(telescope_id)
+
+    # Make sure it is within the range
+    if telescope_id < 1:
+        telescope_id = 1
+    else:
+        telescope_id = (telescope_id - 1) % 4 + 1
+
+    return render(request, "overview/index.html", { "telescope_id": telescope_id })
